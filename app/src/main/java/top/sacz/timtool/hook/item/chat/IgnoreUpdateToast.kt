8584728b6bf1961b00258786052a0ab4f7f2b5bc
Live-Block <liveblock@qq.com>
2025-07-21 23:50:10 +0800

    feat: make the link accessible

---
+++ app/src/main/java/top/sacz/timtool/hook/item/chat/IgnoreUpdateToast.kt
@@ -0,0 +1,23 @@
+package top.sacz.timtool.hook.item.chat
+
+import top.sacz.timtool.hook.base.BaseSwitchFunctionHookItem
+import top.sacz.timtool.hook.core.annotation.HookItem
+import top.sacz.xphelper.reflect.MethodUtils
+
+@HookItem("辅助功能/聊天/移除点击链接 或 小程序时的更新Toast")
+class IgnoreUpdateToast : BaseSwitchFunctionHookItem() {
+    override fun getTip(): String {
+        return "开启此功能可直接点击 链接/小程序 并进入"
+    }
+
+    override fun loadHook(loader: ClassLoader) {
+        val ignToast = MethodUtils.create("com.tencent.mobileqq.aio.msglist.holder.component.ark.d")
+            .methodName("a")
+            .params(String::class.java, String::class.java)
+            .returnType(Boolean::class.javaObjectType)
+            .first()
+        hookAfter(ignToast) { param ->
+            param.result = true
+        }
+    }
+}
